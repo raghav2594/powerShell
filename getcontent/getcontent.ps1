6acd7b7487ea89113acd18229f077977c91093c9
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-04-25 17:25:20 +0530

    GetContent commandlet

---
+++ getcontent/getcontent.ps1
@@ -0,0 +1,15 @@
+﻿#Reads all the content inside the errorLog file
+Get-Content -path "C:\Users\ugh4kor\Documents\Raghav doc\1 Study_Materials\PowerShell\getcontent\errorLog.txt"
+
+#Reads all the content of .txt file which is present inside the getcontent folder
+Get-Content -path "C:\Users\ugh4kor\Documents\Raghav doc\1 Study_Materials\PowerShell\getcontent\*" -Include '*.txt'
+
+#Read first 2 lines of the content from
+Get-Content -path "Documents\Raghav doc\1 Study_Materials\PowerShell\getcontent\errorLog.txt" -First 2
+
+#Read last 2 lines of the content from
+Get-Content -path "Documents\Raghav doc\1 Study_Materials\PowerShell\getcontent\errorLog.txt" -Tail 2
+
+#Read the line which is having failure word
+Get-Content -path "Documents\Raghav doc\1 Study_Materials\PowerShell\getcontent\errorLog.txt" | Where-Object {$_ -like "*fail*"}
+Get-Content -path "Documents\Raghav doc\1 Study_Materials\PowerShell\getcontent\errorLog.txt" | Where-Object {$_ -match "fail"}

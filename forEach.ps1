1250c6bad6aa4bae2a5de67a0d679f7e4f8943ca
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-04-25 13:03:27 +0530

    For loop and foreach loop

---
+++ forEach.ps1
@@ -0,0 +1,12 @@
+﻿$services = @('vm1', 'vm2', 'vm3','vm4')
+
+foreach($service in $services){
+$service
+}
+
+$serverList = Get-Content "C:\Users\ugh4kor\Documents\Raghav doc\1 Study_Materials\PowerShell\serverList.txt"
+
+
+foreach($server in $serverList){
+$server
+}
---
+++ forLoop.ps1
@@ -0,0 +1,17 @@
+﻿for($i = 0; $i -le 10; $i++)
+{
+$i
+}
+
+for(($i = 0),($j = 0); $i -le 10; $i++,$j++)
+{
+"$i" + ',' + "$j"
+}
+
+$serverList = Get-Content "C:\Users\ugh4kor\Documents\Raghav doc\1 Study_Materials\PowerShell\serverList.txt"
+
+$serverList
+
+for($i = 0; $i -lt $serverList.Count; $i++){
+'contents from serverList file is : ' + $serverList[$i]
+}

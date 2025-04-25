110abf13d96ffc30425d1ab70f416d2e97bf8f4d
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-04-25 13:02:16 +0530

    Variables and datatypes

---
+++ VariablesDataTypes.ps1
@@ -0,0 +1,14 @@
+﻿$myVariable = 100.3
+Write-Host $myVariable.GetType()
+
+$myVariable1 = "$(1+1)"
+Write-Host $myVariable1
+
+$myArray = 1,2,3
+$myArray
+Write-Host $myArray[1]
+
+$services = @('vm1', 'vm2', 'vm3','vm4')
+$services[-5]
+$services = Get-Service
+$services[-1]

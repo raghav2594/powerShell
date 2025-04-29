d2c4fd5a340923ff9497681b952b6116178d245e
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-04-29 19:08:01 +0530

    Functions
    * Parameterised function
    * Non-Parameterised function

---
+++ 10_Functions.ps1
@@ -0,0 +1,27 @@
+﻿$a = 10
+$b = 20
+$c = $a + $b
+
+Write-Host "Sum of $a and $b is " $c
+#------------------------------------------
+#Parameterised function
+function sum()
+{
+param($a, $b)  #Parameter
+$c = $a + $b
+Write-Host "Sum of $a and $b is " $c 
+}
+
+#Calling sum function
+sum 33 22
+sum -b 20 -a 10
+#------------------------------------------
+#Non-Parameterised function
+function myprocess()
+{
+Get-Process
+}
+myprocess
+#------------------------------------------
+
+

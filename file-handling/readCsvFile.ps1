3c8bde24fb966fd0d0bd6f1b6ef8db4a82e9444c
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-05-14 12:43:39 +0530

    Read csv file

---
+++ file-handling/readCsvFile.ps1
@@ -0,0 +1,14 @@
+﻿#Import-csv command used to read the data from csv files in the form of tables
+
+Import-Csv "C:\Users\ugh4kor\Documents\Raghav doc\1 Study_Materials\PowerShell\file-handling\serverList_CSV.csv"
+
+$servers = Import-Csv "C:\Users\ugh4kor\Documents\Raghav doc\1 Study_Materials\PowerShell\file-handling\serverList_CSV.csv"
+
+Write-Host $servers
+foreach($server in $servers)
+{
+ $serverName = $server.ServerName
+ $IP = $server.IP
+
+ Write-Host $serverName $ip
+}

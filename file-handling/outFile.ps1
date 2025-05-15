3c8ce5d043148e469300f5222280e97f62a3a4b1
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-05-15 12:15:55 +0530

    Out-File command to save .txt file

---
+++ file-handling/outFile.ps1
@@ -0,0 +1,19 @@
+﻿#Out-File helps to write output directly to a text file
+
+<#Syntax 
+  Out-File [-FilePath] <String>
+           [-Append]
+           [-Force]
+
+-FilePath specify the path to store the data to the file
+-Append helps to append the new data to the existing data
+-Force overRide the readonly file also
+#>
+
+'PowerShell is a very good scripting tool' | Out-File c:/Raghav/test.txt -Append
+#Invoke-Item helps to open the respective file
+Invoke-Item c:/Raghav/test.txt
+
+
+
+

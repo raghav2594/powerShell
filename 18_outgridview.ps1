b20599bdc7f8686680a487bb529dc88d6e2646e9
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-05-16 10:12:04 +0530

    Out-GridView command

---
+++ 18_outgridview.ps1
@@ -0,0 +1,13 @@
+﻿#The Out-GridView cmdlet sends the output from the command to a grid view window where the output is displayed in an interactive table
+
+
+Get-ChildItem C:\Raghav | Out-GridView
+#After execution it opens with GUI window where it will show all the data
+#That window is helpful to filter the data efficiently
+Get-Process | Out-GridView
+
+#Fetch the necessary data from Get-Process and Sort-Object Decending based on PeasWorkingSet
+Get-Process |Select-Object -Property Name, WorkingSet, PeakWorkingSet |Sort-Object -Property PeakWorkingSet -Descending| Out-GridView
+
+Get-ChildItem -Path $PSHOME -Recurse | Out-GridView
+
